5d15fb4d34147527b90aa6d2ea520e559644c19e
Oghenero Ologe <nologe37@gmail.com>
2025-04-12 15:43:15 +0100

    changes to router error config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Buffer } from 'buffer'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import { WagmiProvider } from 'wagmi'
 
 import App from './App.tsx'
@@ -31,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Route path='/explore' element={<Explore />} />
             <Route path='/create-post' element={<CreatePost />} />
             <Route path='/post/:cid' element={<Post />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
